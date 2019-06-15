73060a130cf65f80fc615e4ac5e8e43776beb4fa
Victor Faria Fernandes <victorfariafernandes@gmail.com>
2019-06-15 18:26:43 -0300

    Adicionando  arquivos inciais

---
+++ codigoRosseti.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int t = 2;
+
+typedef struct pizza {
+	int cod;
+	char nome[50];
+	char descricao[20];
+	float preco;
+} TPizza;
+
+typedef struct arvbm{
+  int nchaves, folha;
+  TPizza ** info;
+  struct arvbm **filho, *prox;
+}TABM;
+
+
+TABM *cria(int t){
+  TABM* novo = (TABM*)malloc(sizeof(TABM));
+  novo->nchaves = 0;
+  novo->chave =(int*)malloc(sizeof(int)*((t*2)-1));
+  novo->folha = 1;
+  novo->filho = (TABM**)malloc(sizeof(TABM*)*t*2);
+  novo->prox = NULL;
+  int i;
+  for(i=0; i<(t*2); i++) novo->filho[i] = NULL;
+  return novo;
+}
+
+
+TABM * retira(TABM *a, int mat){
+    return NULL;
+}
+
+
+TABM *inicializa(void){
+  return NULL;
+}
+
+void libera(TABM *a){
+  if(a){
+    if(!a->folha){
+      int i;
+      for(i = 0; i <= a->nchaves; i++) libera(a->filho[i]);
+    }
+    free(a->chave);
+    free(a);
+  }
+}
+
+TABM *busca(TABM *a, int mat){
+  if (!a) return NULL;
+  int i = 0;
+  while ((i < a->nchaves) && (mat > a->chave[i])) i++;
+  if ((i < a->nchaves) && (a->folha) && (mat == a->chave[i])) return a;
+  if (a-> folha) return NULL;
+  if (a->chave[i] == mat) i++;
+  return busca(a->filho[i], mat);
+}
+
+void imprime(TABM *a, int andar){
+  if(a){
+    int i,j;
+    for(i=0; i<=a->nchaves-1; i++){
+      imprime(a->filho[i],andar+1);
+      for(j=0; j<=andar; j++) printf("   ");
+      printf("%d\n", a->chave[i]);
+    }
+    imprime(a->filho[i],andar+1);
+  }
+}
+
+TABM *divisao(TABM *x, int i, TABM* y, int t){
+  TABM *z = cria(t);
+  z->folha = y->folha;
+  int j;
+  if(!y->folha){
+    z->nchaves = t-1;
+    for(j=0;j<t-1;j++) z->chave[j] = y->chave[j+t];
+    for(j=0;j<t;j++){
+      z->filho[j] = y->filho[j+t];
+      y->filho[j+t] = NULL;
+    }
+  }
+  else {
+    z->nchaves = t; //z possuir� uma chave a mais que y se for folha
+    for(j=0;j < t;j++) z->chave[j] = y->chave[j+t-1];//Caso em que y � folha, temos q passar a info para o n� da direita
+    y->prox = z;
+  }
+  y->nchaves = t-1;
+  for(j=x->nchaves; j>=i; j--) x->filho[j+1]=x->filho[j];
+  x->filho[i] = z;
+  for(j=x->nchaves; j>=i; j--) x->chave[j] = x->chave[j-1];
+  x->chave[i-1] = y->chave[t-1];
+  x->nchaves++;
+  return x;
+}
+
+
+TABM *insere_nao_completo(TABM *x, int mat, int t){
+  int i = x->nchaves-1;
+  if(x->folha){
+    while((i>=0) && (mat < x->chave[i])){
+      x->chave[i+1] = x->chave[i];
+      i--;
+    }
+    x->chave[i+1] = mat;
+    x->nchaves++;
+    return x;
+  }
+  while((i>=0) && (mat < x->chave[i])) i--;
+  i++;
+  if(x->filho[i]->nchaves == ((2*t)-1)){
+    x = divisao(x, (i+1), x->filho[i], t);
+    if(mat > x->chave[i]) i++;
+  }
+  x->filho[i] = insere_nao_completo(x->filho[i], mat, t);
+  return x;
+}
+
+TABM *insere(TABM *T, int mat, int t){
+  if(busca(T, mat)) return T;
+  if(!T){
+    T=cria(t);
+    T->chave[0] = mat;
+    T->nchaves=1;
+    return T;
+  }
+  if(T->nchaves == (2*t)-1){
+    TABM *S = cria(t);
+    S->nchaves=0;
+    S->folha = 0;
+    S->filho[0] = T;
+    S = divisao(S,1,T,t);
+    S = insere_nao_completo(S, mat, t);
+    return S;
+  }
+  T = insere_nao_completo(T, mat, t);
+  return T;
+}
+
+
+int main(void){
+  TABM * arvore = inicializa();
+  int num = 0, from, to;
+  while(num != -1){
+    printf("Digite um numero para adicionar. 0 para imprimir e -1 para sair\n");
+    scanf("%i", &num);
+    if(num == -1){
+      printf("\n");
+      imprime(arvore,0);
+      libera(arvore);
+      return 0;
+    }
+    /*
+    else if(num == -9){
+      scanf("%d", &from);
+      arvore = retira(arvore, from, t);
+      Imprime(arvore,0);
+    }
+    */
+    else if(!num){
+      printf("\n");
+      imprime(arvore,0);
+    }
+    else arvore = insere(arvore, num, t);
+    printf("\n\n");
+  }
+}
